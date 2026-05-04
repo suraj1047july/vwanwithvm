e43f119d1fc34671c5cc5e6440dff0eb71ae7a98
suraj1047july <122980832+suraj1047july@users.noreply.github.com>
2026-05-04 15:58:34 +0530

    Update vms_CORRECTED.tf

--- vms_CORRECTED.tf
+++ vms_CORRECTED.tf
@@ -4,7 +4,7 @@ resource "azurerm_windows_virtual_machine" "app1" {
   name                = "vm-app1-${var.environment}"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-  size                = "Standard_FX2msd_v2"
+  size                = "Standard_D2as_v5"
 
   admin_username = var.admin_username
   admin_password = var.admin_password
@@ -36,7 +36,7 @@ resource "azurerm_windows_virtual_machine" "app2" {
   name                = "vm-app2-${var.environment}"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-  size                = "Standard_FX2msd_v2"
+  size                = "Standard_D2as_v5"
 
   admin_username = var.admin_username
   admin_password = var.admin_password
